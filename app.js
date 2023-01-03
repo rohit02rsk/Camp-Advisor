c345566c26df1dd710394d2359efcc87555cd32b
S Rohit Kumar <96235764+rohit02rsk@users.noreply.github.com>
2023-01-03 22:50:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -101,6 +101,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err });
 })
 
-app.listen(3000, () => {
-    console.log("Listening on PORT 3000.");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
 })
