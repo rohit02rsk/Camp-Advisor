bd4768dfe1898b440b05073c182b749123e892f5
S Rohit Kumar <96235764+rohit02rsk@users.noreply.github.com>
2023-01-03 22:31:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const MongoDBStore = require('connect-mongo');
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
-const dbUrl = 'mongodb://localhost:27017/camp-advisor'
+const dbUrl = process.env.DB_URL
 //'mongodb://localhost:27017/camp-advisor'
 mongoose.connect(dbUrl);
 mongoose.set('strictQuery', true);
